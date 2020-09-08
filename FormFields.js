e8b0e8fe833c893e072047bf7c1190efe07bf670
cnpatinog <62357449+cnpatinog@users.noreply.github.com>
2020-09-07 19:10:46 -0500

    Add files via upload

---
+++ FormFields.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+  var sel = $("select.formControl");
+  for (var i=0; i<sel.length;i++){
+    options = $(sel[i]).children();
+    iconInputField = $($($(sel[i]).parent()).parent());
+    iconInputField.after('<div class="selectInputBox"><div class="searchSelectBox"><div class="iconInputField"><div class="inputIcon"><i class="fas fa-search"></i></div><div class="inputField"><input class="formControl" type="text" name="searchSelect" placeholder="Search..."></input></div></div></div><div class="SelectItems"></div></div>');
+    var FormGroup = $(iconInputField.parent());
+    var SelectItems = $(FormGroup.children(".selectInputBox")).children(".SelectItems")
+    for (var j=0;j < options.length;j++){
+      var optionString = '<div class="SelectOption">Option</div>';
+      SelectItems.append(optionString);
+    }
+  }
+})
